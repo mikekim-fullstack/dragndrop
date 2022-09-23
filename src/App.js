6c258843059827e2f2cccd4f1abbbb20324a11f8
Mike Kim <mikekim@MK.local>
2022-09-23 13:33:47 -0400

    init setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [fruitItems, setFruitItems] = useState([
+    { id: 1, name: 'Apple', price: 0.95 },
+    { id: 2, name: 'Banana', price: 1 },
+    { id: 3, name: 'Pear', price: 1.2 },
+    { id: 4, name: 'Graps', price: 4.0 },
+    { id: 5, name: 'Orange', price: 2.0 },
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2 className='title'>Fruit Lists</h2>
+        <div className="input-row">
+          <input type='text' name='fruitName' placeholder='e.g. Banana' />
+          <button className='btn btn-primary'>Add&nbsp;item</button>
+        </div>
       </header>
+
+      {/* Display the kinds of fruit.. */}
+      <div className="list-container">
+        <div className="list-item" draggable>
+          <i className="fa-solid fa-bars"></i>
+          <h3>Apple</h3>
+        </div>
+        <div className="list-item" draggable>
+          <i className="fa-solid fa-bars"></i>
+          <h3>Pear</h3>
+        </div>
+      </div>
     </div>
   );
 }
